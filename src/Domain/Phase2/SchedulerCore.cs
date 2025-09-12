478aabdc7c15ff27facbb6be1222c0addc178136
Azrea-Shade <Azrea-Shade@users.noreply.github.com>
2025-09-12 15:12:58 -0600

    phase2: fix tests: SchedulerCore returns (object,object); add double.HasValue() extension

--- src/Domain/Phase2/SchedulerCore.cs
+++ src/Domain/Phase2/SchedulerCore.cs
@@ -6,16 +6,16 @@ namespace Domain
 {
     public static class SchedulerCore
     {
-        public static (IEnumerable<DateTime> gen, IEnumerable<DateTime> noti)
+        public static (object gen, object noti)
             NextTimes(object expr, object start, object end, object count)
-            => (Enumerable.Empty<DateTime>(), Enumerable.Empty<DateTime>());
+            => ((object)Enumerable.Empty<DateTime>(), (object)Enumerable.Empty<DateTime>());
 
-        public static (IEnumerable<DateTime> gen, IEnumerable<DateTime> noti)
+        public static (object gen, object noti)
             NextTimes(string expr, string start, string end, int count)
-            => (Enumerable.Empty<DateTime>(), Enumerable.Empty<DateTime>());
+            => ((object)Enumerable.Empty<DateTime>(), (object)Enumerable.Empty<DateTime>());
 
-        public static (IEnumerable<DateTime> gen, IEnumerable<DateTime> noti)
+        public static (object gen, object noti)
             NextTimes(string expr, string start, string end, string count)
-            => (Enumerable.Empty<DateTime>(), Enumerable.Empty<DateTime>());
+            => ((object)Enumerable.Empty<DateTime>(), (object)Enumerable.Empty<DateTime>());
     }
 }
